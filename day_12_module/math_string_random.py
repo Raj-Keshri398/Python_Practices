08ff74773b85afa44af52bc443199edbbdca8c06
Raj-Keshri398 <keshriraj093@gmail.com>
2025-12-01 22:34:44 +0530

    knowing about math , string, random module

---
+++ day_12_module/math_string_random.py
@@ -0,0 +1,36 @@
+# ------------------- Random ----------------------
+
+from random import random, randint
+
+print(random())
+print(randint(5, 20))
+
+# ----------------- Math ---------------------------
+
+import math
+print(math.pi)           # 3.141592653589793, pi constant
+print(math.sqrt(4))      # 2.0, square root
+print(math.pow(2, 3))    # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))   # 10, rounding to the highest
+print(math.log10(100))   # 2, logarithm with 10 as base
+
+from math import pi, sqrt, pow, floor, ceil, log10
+print(pi)                 # 3.141592653589793
+print(sqrt(2))            # 1.4142135623730951
+print(pow(2, 3))          # 8.0
+print(floor(9.81))        # 9
+print(ceil(9.81))         # 10
+print(math.log10(100))    # 2
+
+from math import pi as  PI
+print(PI) # 3.141592653589793
+
+
+# ---------------------------- String ------------------------------------
+
+import string
+print(string.ascii_letters) # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(string.digits)        # 0123456789
+print(string.punctuation)   # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
